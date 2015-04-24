d55ec08def93adda31b4275f903e67068d7e4cf2
matayoshi <ta-zan2013@hotmail.co.jp>
2015-04-25 07:49:20 +0900

    xxx

--- src/niotest/Test.java
+++ src/niotest/Test.java
@@ -1,10 +1,44 @@
 package niotest;
 
-public class Test {
+public class Test extends TestCase{
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		// まずは、パスClass操作から
+		/* "interface"
+		CopyOption 	
+		DirectoryStream<T> 	
+		DirectoryStream.Filter<T> 	
+		FileVisitor<T> 	
+		OpenOption 	
+		Path 	
+		PathMatcher 	
+		SecureDirectoryStream<T> 	
+		Watchable 	
+		WatchEvent<T> 	
+		WatchEvent.Kind<T> 	
+		WatchEvent.Modifier 	
+		WatchKey 	
+		WatchService 	
+		*/
+		/* "Class"
+		 File
+		 FileStore
+		 FileSystems
+		 LinkPermission
+		 Paths
+		 SimpleFileVistor
+		 StanderdWatchEventKinds
+		 */
 	}
 
+	@Test
+	public void testNioPath(){
+		/**java.nio.file.Path*/
+		java.nio.file.Path nioPath = new java.nio.file.Path();
+		nioPath.
+		
+		
+		/**java.nio.file.Paths*/
+		
+	}
 }
